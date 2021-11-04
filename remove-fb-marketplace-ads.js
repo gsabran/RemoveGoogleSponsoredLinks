56ed54f11ba834d3265654295626d16de0cba448
Guillaume Sabran <guillaume.sabran@airbnb.com>
2021-11-04 15:24:04 -0700

    remove market place ads

--- remove-fb-marketplace-ads.js
+++ remove-fb-marketplace-ads.js
@@ -1,15 +1,18 @@
 const removeBadContent = () => {
   try {
+    if (!window.location.pathname.includes('marketplace')) {
+      return;
+    }
     const container = document.querySelectorAll('.l9j0dhe7.f9o22wc5.ad2k81qe')[0].children[1];
 
     for (const item of container.children) {
-      if (item.textContent.includes('Sponsored') || item.textContent.includes('Ships to you')) {
+      if (item.textContent.includes('Sponsored')) {
         // item.remove()
         item.innerHTML = '<div style="background-color: white;"></div>'
       }
     }
   } catch (e) {
-    console.error('Error while removing sponsored and non local elements:', e);
+    console.log('Error while removing sponsored and non local elements:', e);
   }
 }
 
